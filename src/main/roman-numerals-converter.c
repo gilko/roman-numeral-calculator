01209ba117d38cb9e5cc6356a564e6ffeb1a5055
Gabe Ilko <gabeilko@gmail.com>
2016-08-14 23:47:59 -0400

    refactor readability

--- src/main/roman-numerals-converter.c
+++ src/main/roman-numerals-converter.c
@@ -9,18 +9,20 @@ char *convert_internal_sum(char *roman_numerals);
 #define NO_INDEX -1
 #define SUBTRACTIVE_PREFIX_ARRAY_SIZE 6
 #define INTERNAL_SUM_ARRAY_SIZE 6
-char *internalSumPrefix[7] = {"IIIII","VV","XXXXX","LL","CCCCC","DD"};
-char *internalSumValue[8] = {"V","X","L","C","D","M"};
-char *subtractivePrefix[7] = {"IV","IX","XL","XC","CD","CM"};
-char *additivePrefix[8] = {"IIII","VIIII","XXXX","LXXXX","CCCC","DCCCC"};
 
 char *convert_subtractive_to_additive_prefix(char *roman_numerals){
+  char *subtractivePrefix[7] = {"IV","IX","XL","XC","CD","CM"};
+  char *additivePrefix[8] = {"IIII","VIIII","XXXX","LXXXX","CCCC","DCCCC"};
+
   int prefixIndex = _findPrefixIndex(subtractivePrefix, SUBTRACTIVE_PREFIX_ARRAY_SIZE, roman_numerals);
 
   return (prefixIndex == NO_INDEX) ? roman_numerals : additivePrefix[prefixIndex];
 }
 
 char *convert_internal_sums(char *romanNumerals){
+  char *internalSumPrefix[7] = {"IIIII","VV","XXXXX","LL","CCCCC","DD"};
+  char *internalSumValue[8] = {"V","X","L","C","D","M"};
+
   int i;
   for (i = 0; i < INTERNAL_SUM_ARRAY_SIZE; i++) {
     if(_numeralsContainsInternalSum(romanNumerals, internalSumPrefix[i])){
