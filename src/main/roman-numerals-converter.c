7a954024cb23b7f51e23cee19fd07e547669523b
Gabe Ilko <gabeilko@gmail.com>
2016-09-22 20:42:04 -0400

    simplify roman-numerals-converter

--- src/main/roman-numerals-converter.c
+++ src/main/roman-numerals-converter.c
@@ -3,11 +3,6 @@
 #include <string.h>
 #include "common-utils.h"
 
-static int findPrefixIndex(char *prefixArray[], int arraySize, char *romanNumerals);
-static int numeralsContainsInternalSum(char *romanNumerals,char *internalSumPrefix);
-static int numeralsContainsAdditivePrefix(char *romanNumerals,char *additivePrefix);
-static int numeralsContainsSubtractivePrefix(char *romanNumerals,char *subtractivePrefix);
-
 #define NO_INDEX -1
 #define SUBTRACTIVE_PREFIX_ARRAY_SIZE 6
 #define ADDITIVE_PREFIX_ARRAY_SIZE 6
@@ -19,22 +14,18 @@ char *convertSubtractiveToAdditivePrefix(char *romanNumerals){
 
   int i;
   for (i = 0; i < SUBTRACTIVE_PREFIX_ARRAY_SIZE; i++) {
-    if(numeralsContainsSubtractivePrefix(romanNumerals, subtractivePrefix[i])){
       romanNumerals = replaceSubstring(romanNumerals, subtractivePrefix[i], additivePrefix[i]);
-    }
   }
   return romanNumerals;
 }
 
 char *convertAdditiveToSubtractivePrefix(char *romanNumerals){
-  char *subtractivePrefix[7] = {"IV","IX","XL","XC","CD","CM"};
   char *additivePrefix[7] = {"IIII","VIIII","XXXX","LXXXX","CCCC","DCCCC"};
+  char *subtractivePrefix[7] = {"IV","IX","XL","XC","CD","CM"};
 
   int i;
   for (i = ADDITIVE_PREFIX_ARRAY_SIZE - 1; i >= 0; i--) {
-    if(numeralsContainsAdditivePrefix(romanNumerals, additivePrefix[i])){
       romanNumerals = replaceSubstring(romanNumerals, additivePrefix[i], subtractivePrefix[i]);
-    }
   }
   return romanNumerals;
 }
@@ -45,32 +36,7 @@ char *convertInternalSums(char *romanNumerals){
 
   int i;
   for (i = 0; i < INTERNAL_SUM_ARRAY_SIZE; i++) {
-    if(numeralsContainsInternalSum(romanNumerals, internalSumPrefix[i])){
       romanNumerals = replaceSubstring(romanNumerals, internalSumPrefix[i], internalSumValue[i]);
-    }
   }
   return romanNumerals;
-}
-
-int numeralsContainsInternalSum(char *romanNumerals,char *internalSumPrefix){
-  return strstr(romanNumerals, internalSumPrefix) != NULL;
-}
-
-int numeralsContainsAdditivePrefix(char *romanNumerals,char *additivePrefix){
-  return strstr(romanNumerals, additivePrefix) != NULL;
-}
-
-int numeralsContainsSubtractivePrefix(char *romanNumerals,char *subtractivePrefix){
-  return strstr(romanNumerals, subtractivePrefix) != NULL;
-
-}
-int findPrefixIndex(char *prefixArray[], int arraySize, char *romanNumerals){
-  int prefixIndex = NO_INDEX;
-  int i;
-  for (i = 0; i < arraySize; i++) {
-    if(strcmp(prefixArray[i],romanNumerals) == 0){
-      prefixIndex = i;
-    }
-  }
-  return prefixIndex;
 }
