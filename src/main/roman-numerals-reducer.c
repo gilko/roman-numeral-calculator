b7717aabb6645a21ee8d62b9c4dc6767994bca97
Gabe Ilko <gabeilko@gmail.com>
2016-10-12 22:54:20 -0400

    refactor reducer for better readability

--- src/main/roman-numerals-reducer.c
+++ src/main/roman-numerals-reducer.c
@@ -5,31 +5,29 @@
 #include "roman-numerals-expander.h"
 #include "constants.h"
 
-static int shouldContinue(const char reduceBy, const int loop);
+static int shouldContinue(const char currentReduceByCharacter, const int loop);
 static void makeResult(const char *partialResult, const int loop, char result[]);
-static void removeCommonNumerals(const char *romanNumeralsCopy, const char *commonNumeral, char partialResult[]);
-static int containsReduceBy(const char *romanNumerals, const char *reduceBy);
+static void removeCommonNumerals(const char *romanNumeralsCopy, const char currentReduceByCharacter, char *partialResult);
+static int containsReduceBy(const char *romanNumeralsCopy, const char currentReduceByCharacter);
 static int MAX_LOOP = 30;
 
-void reduceMatchingNumerals(const char *romanNumerals, const char reduceBy[], char result[]){
+void reduceMatchingNumerals(const char *romanNumerals, const char *reduceByRomanNumerals, char result[]){
   char partialResult[BUFFER_SIZE] = {};
   char romanNumeralsCopy[BUFFER_SIZE] = {};
   strcpy(romanNumeralsCopy, romanNumerals);
 
-  int loop = 0;
   int index = 0;
-  while(shouldContinue(reduceBy[index], loop)) {
-    char reduceByCharacter = reduceBy[index];
-    char reduceByCharacterString[2] = {};
-    reduceByCharacterString[0] = reduceByCharacter;
+  int loop = 0;
+  while(shouldContinue(reduceByRomanNumerals[index], loop)) {
+    char currentReduceByCharacter = reduceByRomanNumerals[index];
     partialResult[0] = '\0';
 
-    if(containsReduceBy(romanNumeralsCopy, reduceByCharacterString)){
-      removeCommonNumerals(romanNumeralsCopy,  reduceByCharacterString, partialResult);
+    if(containsReduceBy(romanNumeralsCopy, currentReduceByCharacter)){
+      removeCommonNumerals(romanNumeralsCopy,  currentReduceByCharacter, partialResult);
       index++;
     }
     else{
-      expandNumerals(romanNumeralsCopy, reduceByCharacter , partialResult);
+      expandNumerals(romanNumeralsCopy, currentReduceByCharacter, partialResult);
     }
 
     loop++;
@@ -39,22 +37,28 @@ void reduceMatchingNumerals(const char *romanNumerals, const char reduceBy[], ch
   makeResult(partialResult, loop, result);
 }
 
-void removeCommonNumerals(const char *romanNumeralsCopy, const char *commonNumeral, char partialResult[]){
-  replaceSubstring(romanNumeralsCopy,  commonNumeral, "", partialResult);
+static int shouldContinue(const char currentReduceByCharacter, const int loop){
+  return currentReduceByCharacter != '\0' && loop < MAX_LOOP;
 }
 
-static int shouldContinue(const char reduceBy, const int loop){
-  return reduceBy != '\0' && loop < MAX_LOOP;
+static int containsReduceBy(const char *romanNumeralsCopy, const char currentReduceByCharacter){
+  char currentReduceByCharacterString[2] = {};
+  currentReduceByCharacterString[0] = currentReduceByCharacter;
+
+  return strstr(romanNumeralsCopy, currentReduceByCharacterString) != NULL ? 1 : 0;
 }
 
-static void makeResult(const const char *partialResult, const const int loop, char result[]){
+static void removeCommonNumerals(const char *romanNumeralsCopy, const char currentReduceByCharacter, char *partialResult){
+  char currentReduceByCharacterString[2] = {};
+  currentReduceByCharacterString[0] = currentReduceByCharacter;
+
+  replaceSubstring(romanNumeralsCopy,  currentReduceByCharacterString, "", partialResult);
+}
+
+static void makeResult(const char *partialResult, const int loop, char result[]){
   if(loop >= MAX_LOOP){
     strcat(result, "REDUCER FAILED");
   }else{
     strcat(result, partialResult);
   }
-}
-
-static int containsReduceBy(const char *romanNumerals, const char *reduceBy){
-  return strstr(romanNumerals, reduceBy) != NULL ? 1 : 0;
 }
